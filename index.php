6f6cc11381b521e1e534283db2ec1ea72f3afbdd
Rodrigo Nardy <rodrigongimenez9@gmail.com>
2026-09-18 09:44:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,9 @@
             
             $telefone = htmlspecialchars($telefone, ENT_QUOTES, "UTF-8"); 
             
-                if (!empty($nome) || !empty($email) || !empty($telefone)) { $mensagem = "Cadastro realizado com sucesso!"; } 
+                if (!empty($nome) || !empty($email) || !empty($telefone)) {
+                    $mensagem = "Cadastro realizado com sucesso! Nome: " . $nome . "; Email: ". $email. "; Telefone: ". $telefone. ";
+                } 
                 
                 else { $mensagem = "Preencha pelo menos um dos campos."; } } 
 
